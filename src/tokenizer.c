0ee8423b4a5efcb3f5a979e49f5f991b060a3fc7
jc-11 <jcgarcia16@miners.utep.edu>
2021-03-01 08:58:05 -0700

    Committing milestone 3 with methods implemented and worked on regarding print tokens and free tokens. Still working on the other ones.

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -25,3 +25,49 @@ int non_space_char(char c){
     return 0;
 
   }
+
+  char *word_start(char *str){
+    
+  }
+
+  char *word_end(char *str){
+    
+  }
+
+  int count_words(char *str){
+    
+  }
+
+  char *copy_str(char *inStr, short len){
+    
+  }
+
+  char **tokenize(char* str){
+    
+  }
+
+  void print_tokens(char **tokens){
+
+    while (*str != " ") {
+
+	printf("%s \n", *str);
+
+	str++;
+    }
+
+  }
+  
+  void free_tokens(char **tokens){
+
+    char** temp = str;
+
+    while (*temp != " "){
+
+      free(*temp);
+      temp++;
+
+    }
+
+    free(temp);
+
+  }
